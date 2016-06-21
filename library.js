eb836931fa0a32fb37de19cae48d68760fee7c70
DeeJay <dheeraj@nagwani.in>
2016-06-21 17:38:37 +0530

    joining groups

--- library.js
+++ library.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var controllers = require('./lib/controllers'),
-		user = module.parent.require('./user'),
-		winston = module.parent.require('winston'),
-		EmailConfirm = {};
+var groups = module.parent.require('./groups'),
+    db = module.parent.require('./database'),
+    meta = module.parent.require('./meta'),
+    winston = module.parent.require('winston'),
+    EmailConfirm = {};
+
+var targetGroup;
 
 EmailConfirm.init = function(params, callback) {
 	var router = params.router,
@@ -13,10 +16,32 @@ EmailConfirm.init = function(params, callback) {
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
-	router.get('/admin/plugins/email-confirm-group', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/email-confirm-group', controllers.renderAdminPage);
+	router.get('/admin/plugins/email-confirm-group', hostMiddleware.admin.buildHeader, EmailConfirm.renderAdminPage);
+	router.get('/api/admin/plugins/email-confirm-group', EmailConfirm.renderAdminPage);
+
+	meta.settings.get('email-confirm-group', function(err, settings) {
+		if (!err && settings && settings.targetGroup) {
+			targetGroup = settings.targetGroup;
+		} else {
+			return winston.error('[plugins/email-confirm-group] Target group not set');
+		}
+	});
 
 	callback();
+};
+
+EmailConfirm.renderAdminPage = function(req, res, next) {
+	db.getSortedSetRevRange('groups:createtime', 0, -1, function(err, groupNames) {
+		if(err) { return next(err); }
+
+		groupNames = groupNames.filter(function(name) {
+			return name.indexOf(':privileges:') === -1 && name !== 'registered-users';
+		});
+
+		groups.getGroupsData(groupNames, function(err, data) {
+			res.render('admin/plugins/email-confirm-group', {groups: data});
+		});
+	});
 };
 
 EmailConfirm.addAdminNavigation = function(header, callback) {
@@ -29,8 +54,10 @@ EmailConfirm.addAdminNavigation = function(header, callback) {
 	callback(null, header);
 };
 
-EmailConfirm.confirm = function(uid, callback) {
-	winston.info('User with uid: ' + uid + ' confirmed email...');
+EmailConfirm.confirm = function(data, callback) {
+	if (targetGroup != null) {
+		groups.join(targetGroup, data.uid);
+	}
 };
 
 module.exports = EmailConfirm;
--- static/templates/admin/plugins/email-confirm-group.tpl
+++ static/templates/admin/plugins/email-confirm-group.tpl
@@ -1,18 +1,17 @@
-<form role="form" class="quickstart-settings">
+<form role="form" class="email-confirm-group-settings">
 	<div class="row">
 		<div class="col-sm-2 col-xs-12 settings-header">General</div>
 		<div class="col-sm-10 col-xs-12">
 			<p class="lead">
-				Adjust these settings. You can then retrieve these settings in code via:
-				<code>meta.settings.get('quickstart');</code>
+				Select a group to move the user to after email confirmation
 			</p>
 			<div class="form-group">
-				<label for="setting-1">Setting 1</label>
-				<input type="text" id="setting-1" name="setting-1" title="Setting 1" class="form-control" placeholder="Setting 1">
-			</div>
-			<div class="form-group">
-				<label for="setting-2">Setting 2</label>
-				<input type="text" id="setting-2" name="setting-2" title="Setting 2" class="form-control" placeholder="Setting 2">
+				<label for="targetGroup">Target Group</label>
+				<select id="targetGroup" name="targetGroup" title="Move to Group" class="form-control">
+				<!-- BEGIN groups -->
+					<option>{groups.displayName}</option>
+				<!-- END groups -->
+				</select>
 			</div>
 		</div>
 	</div>
