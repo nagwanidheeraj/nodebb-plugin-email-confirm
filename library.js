1ed93df311c00abd8dd2d6d1b20c9cf375d07186
DeeJay <dheeraj.nagwani@happiestminds.com>
2016-06-21 10:39:18 +0530

    Changed occurrences of quick start. Added quick check to see if the new hook in NodeBB works.

--- library.js
+++ library.js
@@ -1,31 +1,36 @@
 "use strict";
 
 var controllers = require('./lib/controllers'),
+		user = module.parent.require('./user'),
+		winston = module.parent.require('winston'),
+		EmailConfirm = {};
 
-	plugin = {};
-
-plugin.init = function(params, callback) {
+EmailConfirm.init = function(params, callback) {
 	var router = params.router,
 		hostMiddleware = params.middleware,
 		hostControllers = params.controllers;
-		
+
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
-	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/quickstart', controllers.renderAdminPage);
+	router.get('/admin/plugins/email-confirm-group', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get('/api/admin/plugins/email-confirm-group', controllers.renderAdminPage);
 
 	callback();
 };
 
-plugin.addAdminNavigation = function(header, callback) {
+EmailConfirm.addAdminNavigation = function(header, callback) {
 	header.plugins.push({
-		route: '/plugins/quickstart',
+		route: '/plugins/email-confirm-group',
 		icon: 'fa-tint',
-		name: 'Quickstart'
+		name: 'Email Confirmation Group'
 	});
 
 	callback(null, header);
 };
+
+EmailConfirm.confirm = function(uid, callback) {
+	winston.info('User with uid: ' + uid + ' confirmed email...');
+}
 
 module.exports = plugin;
--- static/lib/admin.js
+++ static/lib/admin.js
@@ -1,18 +1,18 @@
 'use strict';
 /* globals $, app, socket */
 
-define('admin/plugins/quickstart', ['settings'], function(Settings) {
+define('admin/plugins/email-confirm-group', ['settings'], function(Settings) {
 
 	var ACP = {};
 
 	ACP.init = function() {
-		Settings.load('quickstart', $('.quickstart-settings'));
+		Settings.load('email-confirm-group', $('.email-confirm-group-settings'));
 
 		$('#save').on('click', function() {
-			Settings.save('quickstart', $('.quickstart-settings'), function() {
+			Settings.save('email-confirm-group', $('.email-confirm-group-settings'), function() {
 				app.alert({
 					type: 'success',
-					alert_id: 'quickstart-saved',
+					alert_id: 'email-confirm-group-saved',
 					title: 'Settings Saved',
 					message: 'Please reload your NodeBB to apply these settings',
 					clickfn: function() {
